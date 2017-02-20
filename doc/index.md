356101ce12e32c55efa77d416efa4eea3547b42d
fdelavega <fdelavega@conwet.com>
2017-02-20 17:35:49 +0100

    Include initial version of the bae operator

---
+++ doc/index.md
@@ -0,0 +1,17 @@
+## Introduction
+
+This operator retrieves product statistics from the Busines API Ecosystem and creates a chart with them
+
+## Settings
+
+## Wiring
+
+### Input Endpoints
+
+### Output Endpoints
+
+## Usage
+
+## Reference
+
+- [FIWARE Mashup](https://mashup.lab.fiware.org/)
---
+++ js/main.js
@@ -0,0 +1,110 @@
+/*
+ * bae-statistics-chart-generator
+ * https://github.com/aarranz/bae-statistics-chart-generator-operator
+ *
+ * Copyright (c) 2016 CoNWeT Lab., Universidad Politécnica de Madrid
+ * Licensed under the Apache2 license.
+ */
+
+(function (MP) {
+
+    "use strict";
+
+    var bae_api_endpoint = 'DSUsageManagement/api/usageManagement/v2/usage';
+
+    var unit_measure_mapping = {
+        'megabyte': 'Downloads',
+        'millisecond': 'Usage',
+        'call': 'API calls'
+    };
+
+    var parse_query = function parse_query(location) {
+	    var params = {};
+	    location.search.substr(1).split('&').forEach(function (param) {
+	        var parts = param.split('=');
+	        params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+	    });
+	    return params;
+    };
+
+    var send_chart_options = function send_chart_options(usage_logs) {
+        var series = {}, series_list;
+
+        usage_logs.forEach(function (usage_log) {
+            var info, timestamp, measureName;
+
+            timestamp = Date.parse(usage_log.date);
+            info = {};
+
+            usage_log.usageCharacteristic.forEach(function (entry) {
+                info[entry.name] = entry.value;
+            });
+
+            measureName = unit_measure_mapping[info.unit];
+            if (!(measureName in series)) {
+                series[measureName] = {
+                    name: measureName,
+                    tooltip: {
+                        valueSuffix: ' ' + info.unit
+                    },
+                    data: []
+                };
+            }
+            series[measureName].data.push([timestamp, parseFloat(info.value)]);
+        });
+
+        // Sort series
+        for (var key in series) {
+            series[key].data.sort(function (a, b) {
+                // Sort by timestamp (field 0)
+                return a[0] - b[0];
+            });
+        }
+
+        // Convert series into an array
+        series_list = Object.keys(series).sort().map(function (key) {return series[key];});
+
+        var title = "Usage data for product " + params.productId;
+        var options = {
+            title: {
+                text: title
+            },
+            plotOptions: {
+                series: {
+                    zoneAxis: 'x',
+                    zones: [{
+                        value: new Date().getTime()
+                    }, {
+                        dashStyle: 'dot'
+                    }]
+                }
+            },
+            xAxis: {
+                type: 'datetime',
+                ordinal: false
+            },
+            series: series_list
+        };
+
+        MP.wiring.pushEvent('chart-options', JSON.stringify(options));
+    };
+
+    // Parse bae parameters
+    var params = parse_query(window.frameElement.ownerDocument.location);
+
+    MP.http.makeRequest((new URL(bae_api_endpoint, params.server).toString()), {
+        method: 'GET',
+        requestHeaders: {
+	        "Authorization": "Bearer " + params.token
+	    },
+        parameters: {
+            "usageCharacteristic.value": params.productId,
+            "status": "Guided,Rated"
+        },
+        onSuccess: function (response) {
+            var usage_logs = JSON.parse(response.responseText);
+            send_chart_options(usage_logs);
+        }
+    });
+
+})(MashupPlatform);
